1a8fd601fc9ba6c6985c069509eb66ec0f41e722
Soufiane KARROUMI <soufianekarroumi@outlook.com>
2021-10-27 00:45:52 +0100

    Setup of Configuration Service

---
+++ Backend/OpenERP/services/configuration/src/test/java/ma/aui/openerp/services/configuration/ConfigurationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.aui.openerp.services.configuration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
