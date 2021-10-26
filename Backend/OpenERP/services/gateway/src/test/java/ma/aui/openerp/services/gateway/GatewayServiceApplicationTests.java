52b71fc9b3cc5eba5e1febfb9b83fd03bfebf722
Soufiane KARROUMI <soufianekarroumi@outlook.com>
2021-10-26 22:44:35 +0100

    API Gateway Initial Setup

---
+++ Backend/OpenERP/services/gateway/src/test/java/ma/aui/openerp/services/gateway/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.aui.openerp.services.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
